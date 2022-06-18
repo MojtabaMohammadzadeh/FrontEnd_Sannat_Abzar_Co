f60e52e8e3a140a971ebef438f098b180b47da74
avatrezaei <@00d$ife>
2022-06-18 19:44:13 +0430

    brand api

--- src/pages/brand.vue
+++ src/pages/brand.vue
@@ -1,20 +1,53 @@
 <template>
-  <h1>this is a brand</h1>
+  <h1>{{name}}</h1>
 </template>
 
 <script>
 import { defineComponent } from "@vue/composition-api"; 
+import { api } from "boot/axios";
 
 export default defineComponent({
   name: "m-brand",
   data() {
-    return {};
+    return {
+      name : '',
+    };
+  },
+  computed: {
+    id() {
+      return this.$route.params.id;
+    },
   },
   methods: {
-     
+     getBrandInfo() {
+       var data = new FormData();
+       data.append('token', 'B49K61mY');
+       data.append('page_param', '1');
+       data.append('per_param', '10');
+       data.append('id_brand', this.id);
+ 
+       var config = {
+         method: 'post',
+         url: '/singlebrand',
+         headers: {},
+         data : data
+       };
+ 
+       return api(config).then(response => {
+         let brand = response.data.single_brand;
+         this.name = brand.name; 
+       }).catch(error => {
+         console.log(error);
+       });
+       
+     },
   },
-  mounted() { },
-  setup() {},
+  mounted() { 
+    this.getBrandInfo();
+  },
+  setup() {
+
+  },
 });
 </script>
 
